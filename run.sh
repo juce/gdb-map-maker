affaa59f0e851c132be5188f07712e1325c55b8f
Anton Jouline <juce66@gmail.com>
2018-04-09 11:46:46 -0700

    starting work on the map-maker

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+make build
+java -cp dist mapmaker.MapMaker
---
+++ src/mapmaker/MapMaker.java
@@ -0,0 +1,83 @@
+package mapmaker;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Cursor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.URL;
+import javax.swing.ImageIcon;
+import javax.swing.JCheckBox;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JTabbedPane;
+
+
+public class MapMaker extends JFrame
+{
+    JTabbedPane tabbedPane;
+    PlayersPanel playersPanel;
+    StadiumsPanel stadiumsPanel;
+    BallsPanel ballsPanel;
+
+    public MapMaker() {
+        super("GDB Map Maker");
+        setIcon();
+        buildMenu();
+
+        playersPanel = new PlayersPanel();
+        stadiumsPanel = new StadiumsPanel();
+        ballsPanel = new BallsPanel();
+
+        tabbedPane = new JTabbedPane();
+        tabbedPane.addTab("Players", null, playersPanel, null);
+        tabbedPane.addTab("Stadiums", null, stadiumsPanel, null);
+        tabbedPane.addTab("Balls", null, ballsPanel, null);
+ 
+        getContentPane().add(tabbedPane);
+        pack();
+        setVisible(true);
+    }
+
+    private void buildMenu() {
+        JMenuBar mb = new JMenuBar();
+        JMenu menu = new JMenu("File");
+        JMenuItem exitItem = new JMenuItem("Exit");
+        exitItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent paramAnonymousActionEvent)
+            {
+            System.exit(0);
+            }
+        });
+        menu.add(exitItem);
+        mb.add(menu);
+        setJMenuBar(mb);
+    }
+ 
+    private void setIcon() {
+        URL localURL = getClass().getResource("data/icon.png");
+        if (localURL != null) {
+            ImageIcon localImageIcon = new ImageIcon(localURL);
+            setIconImage(localImageIcon.getImage());
+        }
+    }
+          
+    public static void main(String args[]) {
+        new MapMaker();
+    }
+}
