20ea3c6e0981d72e08d1e41eb8929ff4b5218ba6
Anton Jouline <juce66@gmail.com>
2018-04-16 11:07:40 -0700

    adding startup sequence

--- src/mapmaker/MapMaker.java
+++ src/mapmaker/MapMaker.java
@@ -26,7 +26,100 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JTabbedPane;
+import javax.swing.BoxLayout;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
 
+
+class MapMakerStartup extends JFrame
+{
+    OptionFile of;
+    JProgressBar pb;
+    JPanel loadingPane;
+    List<Listener> listeners;
+    boolean doneLoading;
+
+    public interface Listener {
+        public void loaded(OptionFile of);
+    }
+
+    public MapMakerStartup() {
+        super("GDB Map Maker Loading ...");
+        listeners = new ArrayList<Listener>();
+        setIcon();
+        loadingPane = new JPanel();
+        loadingPane.setLayout(new BoxLayout(loadingPane, BoxLayout.Y_AXIS));
+        pb = new JProgressBar();
+        pb.setValue(0);
+        URL localURL = getClass().getResource("data/icon.png");
+        if (localURL != null) {
+            ImageIcon localImage = new ImageIcon(localURL);
+            loadingPane.add(new JLabel(localImage));
+        }
+        loadingPane.add(new JLabel("Loading, please wait ..."));
+        loadingPane.add(pb);
+        getContentPane().add(loadingPane);
+        pack();
+        setVisible(true);
+        doneLoading = false;
+
+        // progress bar updater
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                while (!doneLoading) {
+                    try { Thread.sleep(20); }
+                    catch (InterruptedException e) {}
+                    pb.setValue((pb.getValue() + 1) % 100);
+                }
+            }
+        });
+        t1.start();
+
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                log("starting");
+                loadOptionFile("m17/KONAMI-WIN32WE9KOPT");
+                log("option file loaded");
+                // notify listeners
+                for (Listener li : listeners) {
+                    li.loaded(of);
+                }
+                log("listeners done");
+                setVisible(false);
+                doneLoading = true;
+            }
+        });
+        t.start();
+
+    }
+
+    public void log(String message) {
+        Date date = new Date();
+        System.out.println(String.format("%s: %s", date, message));
+    }
+
+    private void setIcon() {
+        URL localURL = getClass().getResource("data/icon.png");
+        if (localURL != null) {
+            ImageIcon localImageIcon = new ImageIcon(localURL);
+            setIconImage(localImageIcon.getImage());
+        }
+    }
+
+    public void loadOptionFile(String filename) {
+        of = new OptionFile();
+        of.readXPS(new File(filename));
+    }
+
+    public OptionFile getOptionFile() {
+        return of;
+    }
+
+    public void onStartupDone(Listener listener) {
+        listeners.add(listener);
+    }
+}
 
 public class MapMaker extends JFrame
 {
@@ -35,12 +128,12 @@ public class MapMaker extends JFrame
     StadiumsPanel stadiumsPanel;
     BallsPanel ballsPanel;
 
-    public MapMaker() {
+    public MapMaker(OptionFile of) {
         super("GDB Map Maker");
         setIcon();
         buildMenu();
 
-        playersPanel = new PlayersPanel();
+        playersPanel = new PlayersPanel(of);
         stadiumsPanel = new StadiumsPanel();
         ballsPanel = new BallsPanel();
 
@@ -77,6 +170,10 @@ public class MapMaker extends JFrame
     }
           
     public static void main(String args[]) {
-        new MapMaker();
+        new MapMakerStartup().onStartupDone(new MapMakerStartup.Listener() {
+            public void loaded(OptionFile of) {
+                new MapMaker(of);
+            }
+        });
     }
 }
